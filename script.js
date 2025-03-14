0f211de9e81b313049bddff7e2319db689db0d65
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-03-14 22:06:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-//your JS code here. If required.
 function daysOfAYear(year) {
-  	if(year % 4 == 0){
-		if(year % 400 == 0){
-			if(year % 400 == 0){
-				return 366
-			}else{
-				return 365
-			}
-		}else{
-			return 366
-		} else{
-		return 365;
-		}
-	}
+  // Leap year is divisible by 4, but not by 100, unless also divisible by 400
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return 366; // Leap year
+  } else {
+    return 365; // Non-leap year
+  }
+}
 
-
-} 
+// Test cases
+console.log(daysOfAYear(2022)); // 365
+console.log(daysOfAYear(2024)); // 366
+console.log(daysOfAYear(1900)); // 365
+console.log(daysOfAYear(2000)); // 366
