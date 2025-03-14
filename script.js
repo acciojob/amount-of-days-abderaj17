117af3b41ef0e0e8254b1e9b50a714144988a72a
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-03-14 21:45:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 //your JS code here. If required.
-function daysOfAYear() {
-  	
+function daysOfAYear(year) {
+  	if(year % 4 == 0){
+		if(year % 400 == 0){
+			if(year % 400 == 0){
+				return 366
+			}else{
+				return 365
+			}
+		}else{
+			return 366
+		} else{}
+	}
 }
